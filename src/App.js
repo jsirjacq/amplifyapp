e87aba31c4214bb2b954a44507fcbb024fee349b
Justin Sirjacques <justin.sirjacques@external.toyota-europe.com>
2020-11-17 17:51:42 +0100

    Changing lqnding pqge <p>

--- src/App.js
+++ src/App.js
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bonjour Lieutenant Colonel Bertrand.
+          Votre site web est en cours de développement
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
