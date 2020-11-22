1157e0a6c1e7929636561a6c95127caf754eae1b
Justin Sirjacques <justin.sirjacques@external.toyota-europe.com>
2020-11-22 17:05:33 +0100

    deleting amplify authentication and .idea

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
     return (
         <div className="App">
-            <header>
+            <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <h1>We now have Auth!</h1>
+                <p>
+                    Bonjour Lieutenant Colonel Bertrand.
+                    Votre site web est en cours de développement
+                </p>
             </header>
-            <AmplifySignOut />
         </div>
     );
 }
 
-export default withAuthenticator(App);
+export default App;
